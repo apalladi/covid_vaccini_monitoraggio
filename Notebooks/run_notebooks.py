64f3fbf436520db278a65f66afef52e6fae7ab6b
enricocid <enrico2588@gmail.com>
2021-10-02 18:02:26 +0200

    run_notebooks.py: uniforma stile codice
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- Notebooks/run_notebooks.py
+++ Notebooks/run_notebooks.py
@@ -11,18 +11,18 @@ def run_notebook(nb_path):
     nb_path: notebook path
     return: None'''
 
-    print(f'\nRunning {nb_path} ...')
+    print(f"\nRunning {nb_path} ...")
 
     # open the notebook
-    with open(nb_path, encoding='utf-8-sig') as f:
+    with open(nb_path, encoding="utf-8-sig") as f:
         nb = nbformat.read(f, as_version=nbformat.NO_CONVERT)
 
     # execute the notebook
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
     ep.preprocess(nb)
 
     # update the notebook
-    with open(nb_path, 'w', encoding='utf-8-sig') as f:
+    with open(nb_path, "w", encoding="utf-8-sig") as f:
         nbformat.write(nb, f)
 
 
@@ -30,8 +30,7 @@ def run_notebook(nb_path):
 scriptpath = path.dirname(path.realpath(__file__))
 chdir(scriptpath)
 
-
 # Finally, update notebooks/results
-notebooks = glob('*.ipynb')
+notebooks = glob("*.ipynb")
 for nb in notebooks:
     run_notebook(nb)
