d13d951773c8c227ae7463ebf256d3e4f7105803
enricocid <enrico2588@gmail.com>
2021-10-07 10:31:40 +0200

    Pulizia import e warnings pylint/flake8
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- Notebooks/custom/watermarks.py
+++ Notebooks/custom/watermarks.py
@@ -4,23 +4,26 @@ from PIL import ImageFont
 import numpy as np
 from datetime import datetime
 
+
 def watermark_specs(figure, watermark):
     # Get the default Matplotlib font
-    default_font = findfont(FontProperties(family=['sans-serif']))
-    
+    default_font = findfont(FontProperties(family=["sans-serif"]))
+
     # Fraction of the image where the Watermark is placed
     figure_fraction = 0.6
-    
+
     # Get Figure width, height (Pixels)
     figure_wd = figure.get_figwidth() * figure.dpi
     figure_ht = figure.get_figheight() * figure.dpi
-    
+
     # Get Watermark width (Pixels)
     watermark_wd = int(figure_fraction * figure_wd)
 
-    # Calculate the scaled fontsize - iterate until the text size is larger than the text width and take the max value
-    fontsize = max(takewhile(lambda i: ImageFont.truetype(default_font, i).getsize(watermark)[0] < watermark_wd, 
-                    range(1, watermark_wd))) - 1
+    # Calculate the scaled fontsize - iterate until the text size is
+    # larger than the text width and take the max value
+    fontsize = max(takewhile(lambda i: ImageFont.truetype(default_font, i)
+                   .getsize(watermark)[0] < watermark_wd,
+                   range(1, watermark_wd))) - 1
     # Calculate appropriate rotation and convert to degree (r * 180°/pi)
     angle = np.arctan(figure_ht/figure_wd)*(180/np.pi)
 
@@ -30,31 +33,33 @@ def watermark_specs(figure, watermark):
     print(f"watermark fontsize: {fontsize}, angle:{round(angle, 2)}")
     return fontsize, angle
 
+
 def add_watermark(figure, axis_font_size):
-    watermark = 'github.com/apalladi/covid_monitoraggio_vaccini'
+    watermark = "github.com/apalladi/covid_monitoraggio_vaccini"
     # Get the scaled watermark fontsize and angle
     fontsize, angle = watermark_specs(figure, watermark)
-    figure.text(0.5, 
-                0.5, 
+    figure.text(0.5,
+                0.5,
                 watermark,
-                fontsize=fontsize, 
-                color='gray', 
+                fontsize=fontsize,
+                color="gray",
                 alpha=0.25,
-                ha='center', 
-                va='center', 
-                rotation=angle, 
+                ha="center",
+                va="center",
+                rotation=angle,
                 zorder=0
                 )
 
     # explicit link and last update date at the bottom
-    last_update = datetime.today().strftime('%d-%m-%Y %H:%M')
-    watermark_btm = r'$\bf{Fonte:}$ https://github.com/apalladi/covid_vaccini_monitoraggio'
-    watermark_btm += f'\n Ultimo aggiornamento: {last_update}'
-    figure.text(0.5, 
-                -0.06, 
+    last_update = datetime.today().strftime("%d-%m-%Y %H:%M")
+    watermark_btm = r"$\bf{Fonte:}$ "
+    watermark_btm += "https://github.com/apalladi/covid_vaccini_monitoraggio"
+    watermark_btm += f"\n Ultimo aggiornamento: {last_update}"
+    figure.text(0.5,
+                -0.06,
                 watermark_btm,
                 fontsize=axis_font_size,
-                color='darkslategray', 
-                ha="center", 
+                color="darkslategray",
+                ha="center",
                 va="bottom"
                 )
