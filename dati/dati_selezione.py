09a73d4633e90e2fbbe7ef603c0dcc62edb9e110
Andrea <andrea.palladino@outlook.it>
2021-10-08 12:32:06 +0200

    aggiunti dati precedenti a 8 Settembre

--- dati/dati_selezione.py
+++ dati/dati_selezione.py
@@ -70,68 +70,73 @@ chdir(scriptpath)
 - Automatic (auto = True): table 3 of last available PDF is automatically read
 - Manual: you have to specify PDF link and report date """
 
-# Define mode
-auto = True
+def get_data_from_report(auto=True, table_index=2):
 
-# Replace with index of table of interest
-table_index = 2
+    if auto:
+        # Get most recent report url
+        url_ = get_surveillance_reports()[0]
+    else:
+        # Replace with most recent report url
+        url_ = "https://www.epicentro.iss.it/coronavirus/bollettino/"
+        date = input('Inserisci la data, nel formato giorno-mese (es. 4-agosto) ')
+        url_ += "Bollettino-sorveglianza-integrata-COVID-19_"+str(date)+"-2021.pdf"
 
-if auto:
-    # Get most recent report url
-    url_ = get_surveillance_reports()[0]
-else:
-    # Replace with most recent report url
-    url_ = "https://www.epicentro.iss.it/coronavirus/bollettino/"
-    url_ += "Bollettino-sorveglianza-integrata-COVID-19_29-settembre-2021.pdf"
+    # Get report date
+    rep_date = date_from_url(url_)
+    print(f"\nLatest report ({rep_date.date()}) is:\n{url_}")
 
-# Get report date
-rep_date = date_from_url(url_)
-print(f"\nLatest report ({rep_date.date()}) is:\n{url_}")
+    # Read all tables
+    raw_tb = read_pdf(url_, pages="all", stream=True, silent=True)
 
-# Read all tables
-raw_tb = read_pdf(url_, pages="all", stream=True, silent=True)
+    # keep the last and the third last column
+    try:
+        columns_to_keep = raw_tb[table_index].columns[[-3, -1]]
+    except:
+        print('L\'indice della tabella non è corretta. Scelte consigliate: 2 o 3')
+        
+    
+    to_exclude = r"\((.*)|[^a-z-0-9]|\d+-\d+|\d+\+"
 
-# keep the last and the third last column
-columns_to_keep = raw_tb[table_index].columns[[-3, -1]]
-to_exclude = r"\((.*)|[^a-z-0-9]|\d+-\d+|\d+\+"
+    df = raw_tb[table_index][columns_to_keep]
+    df = df.replace(to_exclude, "", regex=True).replace("", np.nan)
+    df = df.dropna(subset=columns_to_keep, how="all").fillna(0).astype(np.int64)
+    df.columns = ["Non vaccinati", "Immunizzati"]
 
-df = raw_tb[table_index][columns_to_keep]
-df = df.replace(to_exclude, "", regex=True).replace("", np.nan)
-df = df.dropna(subset=columns_to_keep, how="all").fillna(0).astype(np.int64)
-df.columns = ["Non vaccinati", "Immunizzati"]
+    # Get data
+    # Sum value by age/event
+    step_ = 4  # groups (=5) are 4 rows (=20) distant (see foo.pdf)
+    results = [df[col][i:i+step_].sum()
+               for i in np.arange(0, len(df)-step_+1, step_) for col in df.columns]
 
-# Get data
-# Sum value by age/event
-step_ = 4  # groups (=5) are 4 rows (=20) distant (see foo.pdf)
-results = [df[col][i:i+step_].sum()
-           for i in np.arange(0, len(df)-step_+1, step_) for col in df.columns]
+    # Read the original general data csv from apalladi"s repo
+    # https://github.com/apalladi/covid_vaccini_monitoraggio/tree/main/dati
+    repo_url = "https://raw.githubusercontent.com/apalladi/covid_"
+    repo_url += "vaccini_monitoraggio/main/dati/dati_ISS_complessivi.csv"
+    df_0 = pd.read_csv(repo_url,
+                       sep=";",
+                       parse_dates=["data"],
+                       date_parser=date_parser,
+                       index_col="data"
+                       )
 
-# Read the original general data csv from apalladi"s repo
-# https://github.com/apalladi/covid_vaccini_monitoraggio/tree/main/dati
-repo_url = "https://raw.githubusercontent.com/apalladi/covid_"
-repo_url += "vaccini_monitoraggio/main/dati/dati_ISS_complessivi.csv"
-df_0 = pd.read_csv(repo_url,
-                   sep=";",
-                   parse_dates=["data"],
-                   date_parser=date_parser,
-                   index_col="data"
-                   )
+    # Add the new row at the top of the df
+    df_0.loc[rep_date] = results
+    df_0.sort_index(ascending=False, inplace=True)
 
-# Add the new row at the top of the df
-df_0.loc[rep_date] = results
-df_0.sort_index(ascending=False, inplace=True)
+    # Save to a csv
+    df_0.to_csv("dati_ISS_complessivi.csv", sep=";")
 
-# Save to a csv
-df_0.to_csv("dati_ISS_complessivi.csv", sep=";")
+    # Get data by age
+    ages = ["12-39", "40-59", "60-79", "80+"]
+    results_ = {age: df[ages.index(age)::step_].stack().values for age in ages}
 
-# Get data by age
-ages = ["12-39", "40-59", "60-79", "80+"]
-results_ = {age: df[ages.index(age)::step_].stack().values for age in ages}
+    # Load dict as df
+    df_1 = pd.DataFrame(results_).T
+    df_1.columns = df_0.columns
+    df_1.index.rename("età", inplace=True)
 
-# Load dict as df
-df_1 = pd.DataFrame(results_).T
-df_1.columns = df_0.columns
-df_1.index.rename("età", inplace=True)
+    # Save to csv
+    df_1.to_csv(f"data_iss_età_{rep_date.date()}.csv", sep=";")
 
-# Save to csv
-df_1.to_csv(f"data_iss_età_{rep_date.date()}.csv", sep=";")
+
+get_data_from_report()
