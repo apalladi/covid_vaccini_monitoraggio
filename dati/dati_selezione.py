0632c2c39f6ba9da57764284955f904593d4877d
enricocid <enrico2588@gmail.com>
2021-10-25 18:21:20 +0200

    Revisione dati_selezione.py
    Da ora verrà utilizzato camelot e la pagina contenente la tabella
    
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- dati/dati_selezione.py
+++ dati/dati_selezione.py
@@ -2,87 +2,121 @@
 """ dati_selezione.ipynb
 
 Extraction of table 3 from ISS weekly covid-19 reports
-https://www.epicentro.iss.it/coronavirus/sars-cov-2-sorveglianza-dati
+https://www.epicentro.iss.it/coronavirus/aggiornamenti
 
 See example pdf:
 https://www.epicentro.iss.it/coronavirus/bollettino/Bollettino-sorveglianza-integrata-COVID-19_8-settembre-2021.pdf
 
 Requirements:
-Java 8+, Python 3.6+, numpy, pandas, tabula-py, requests, Beautiful Soup 4 """
+Python 3.6+, Ghostscript (ghostscript), Tkinter (python3-tk)
+numpy, pandas, camelot, requests, Beautiful Soup 4 """
 
 
 import locale
+import re
 from datetime import datetime
+from io import BytesIO
 from os import chdir, path
-from re import findall
 from urllib.parse import urljoin
 
+import camelot
 import numpy as np
 import pandas as pd
+import pdfplumber
+import requests
 from bs4 import BeautifulSoup
-from requests import get
-from tabula import read_pdf
 
 
 def get_surveillance_reports():
-    '''get_surveillance_reports() -> list
+    """get_surveillance_reports() -> list
 
-    return: list of "integrated surveillance of Covid-19 in Italy" reports'''
+    return: list of "integrated surveillance of Covid-19 in Italy" reports"""
 
     # Source of the ISS reports
     epicentro_url = "https://www.epicentro.iss.it/coronavirus/aggiornamenti"
     # Requests URL and get response object
-    response = get(epicentro_url)
-    # Parse text obtained
-    soup = BeautifulSoup(response.text, "html.parser")
-    # Find all hyperlinks present on webpage
-    links = soup.find_all("a")
-    # table 3 is available since 14/07/2021
-    cut_date = pd.to_datetime('2021-07-14')
+    with requests.get(epicentro_url) as response:
+        # Parse text obtained
+        soup = BeautifulSoup(response.text, "html.parser")
+        # Find all hyperlinks present on webpage
+        links = soup.find_all("a")
+        # Table 3 is available since 14/07/2021
+        cut_date = pd.to_datetime('2021-07-14')
     return [urljoin(epicentro_url, link["href"]) for link in links
             if ("Bollettino-sorveglianza-integrata-COVID-19" in link["href"])
             and (date_from_url(link["href"], False) >= cut_date)]
 
 
-def date_from_url(sel_url, is_raw):
-    '''date_from_url(str) -> datetime
+def page_from_url(sel_url,
+                  s="TABELLA 3 – POPOLAZIONE ITALIANA DI ETÀ >12 ANNI"):
+    """page_from_url(sel_url) -> int
 
     sel_url: url of the report
-    return: raw date or datetime extracted from report url'''
+    s: search string
+    return: number of the page containing table 3"""
 
-    date_ = findall(r"\d+[a-z-A-Z]+\d+", sel_url)[0]
+    with requests.get(sel_url) as response:
+        # Get raw data from url
+        raw_data = response.content
+        with BytesIO(raw_data) as data:
+            # Open the pdf file
+            pdf = pdfplumber.open(data)
+            # Get number of pages
+            num_pages = len(pdf.pages)
+            print(f"\nNum. of pages is: {num_pages}",
+                  f"\nSearching for: {s}")
+            # Extract text and do the search
+            found_page = None
+            for i in range(num_pages):
+                text = pdf.pages[i].extract_text()
+                if re.search(s, text, re.IGNORECASE):
+                    found_page = i
+                    break
+    return found_page + 1 if found_page is not None else None
+
+
+def date_from_url(sel_url, is_raw):
+    """date_from_url(str) -> datetime
+
+    sel_url: url of the report
+    return: raw date or datetime extracted from report url"""
+
+    date_ = re.findall(r"\d+[a-z-A-Z]+\d+", sel_url)[0]
     return date_ if is_raw else datetime.strptime(date_, "%d-%B-%Y")
 
 
 def date_parser(x):
-    '''date_parser(object) -> datetime
+    """date_parser(object) -> datetime
 
     x: dataframe object
-    return: converts argument to datetime'''
+    return: converts argument to datetime"""
 
     return pd.to_datetime(x, format="%Y/%m/%d")
 
 
-def get_data_from_report(auto=True, table_index=2):
-    ''' get_data_from_report(boolean, int) -> None
+def get_data_from_report(auto=True):
+    """get_data_from_report(boolean, int) -> None
 
     The script saves data extracted from report.
 
     Select mode:
     - Automatic (auto = True): table of last available PDF
                                is automatically read
-    - Manual (auto = False): Index of the report will be asked as input'''
+    - Manual (auto = False): Index of the report will be asked as input"""
 
+    error_msg = "Impossibile estrarre i dati! Procedi manualmente!"
+
+    # Get reports
     reports = get_surveillance_reports()
 
     if auto:
         # Get most recent report url
         rep_url = reports[0]
     else:
-        # build dictionary for manual mode
+        # Build dictionary for manual mode
         reports_dict = dict(enumerate([date_from_url(report, True)
                             for report in reports]))
-        # Select report's index as input
+        # Select report index as input
         rep_idx = input(f'\nScegli indice report:\n\n{reports_dict}\n\n')
         rep_url = reports[int(rep_idx)]
 
@@ -90,44 +124,54 @@ def get_data_from_report(auto=True, table_index=2):
     rep_date = date_from_url(rep_url, False)
     print(f"\nSelected report ({rep_date.date()}) is:\n{rep_url}")
 
-    # Read all tables
-    raw_tables = read_pdf(rep_url, pages="all", stream=True, silent=True)
+    # Get table 3 page number
+    table_page = page_from_url(rep_url)
 
-    # keep the last and the third last column
-    try:
-        # check for errors first, change table_index accordingly
-        raw_tables[table_index].columns[[-3, -1]]
-    except IndexError:
-        # common indexes are 2 and 3, try with 3
-        idx_err_msg = "\nIndice tabella incorretto."
-        idx_err_msg += " Provo con l'indice 3"
-        print(idx_err_msg)
-        table_index = 3
+    # Try a different pattern is no page is found
+    if table_page is None:
+        print("Qualcosa è andato storto! Provo ancora!")
+        # Older report?
+        new_s = "TABELLA 7 – COPERTURA VACCINALE NELLA"
+        new_s += " POPOLAZIONE ITALIANA DI ETÀ >12 ANNI"
+        table_page = page_from_url(rep_url,
+                                   s=new_s)
+        # Can't really find the page, stop
+        if table_page is None:
+            print(error_msg)
+            exit()
 
-    sel_raw_table = raw_tables[table_index]
+    print("\nFound page is:", table_page)
 
-    columns_to_keep = sel_raw_table.columns[[-3, -1]]
+    # Read the found page using camelot
+    df_raw = camelot.read_pdf(rep_url,
+                              pages=f"{table_page}",
+                              flavor="stream")[0].df
 
-    to_exclude = r"\((.*)|[^a-z-0-9]|\d+-\d+|\d+\+"
-
-    df_raw = sel_raw_table[columns_to_keep]
-    df = df_raw.replace(to_exclude, "", regex=True).replace("", np.nan)
-    df = df.dropna(subset=columns_to_keep, how="all")
-
-    try:
-        df = df.fillna(0).astype(np.int64)
-    except ValueError:
-        print("\nImpossibile estrarre dati dal report! Procedi manualmente!")
+    # Check if there are enough columns
+    if len(df_raw.columns) < 3:
+        # Table is incomplete, bye bye
+        print(error_msg)
         exit()
 
-    df.columns = ["Non vaccinati", "Immunizzati"]
+    # keep the last and the third last column
+    columns_to_keep = df_raw.columns[[-3, -1]]
+    df_raw = df_raw[columns_to_keep]
+    df_raw.columns = ["Non vaccinati", "Immunizzati"]
+
+    # Get rows containing the following pattern # (# %)
+    to_find = r"[0-9]|\((.*)"
+    df_raw = df_raw[df_raw["Non vaccinati"].str.match(to_find)]
+
+    # Remove dots and parentheses
+    to_exclude = r"\((.*)|[^0-9]"
+    df_final = df_raw.replace(to_exclude, "", regex=True).astype(np.int64)
 
     # Get data
     # Sum value by age/event
     step_ = 4  # groups (=5) are 4 rows (=20) distant (see foo.pdf)
-    results = [df[col][i:i+step_].sum()
-               for i in np.arange(0, len(df)-step_+1, step_)
-               for col in df.columns]
+    results = [df_final[col][i:i+step_].sum()
+               for i in np.arange(0, len(df_final)-step_+1, step_)
+               for col in df_final.columns]
 
     # Read the original general data csv from apalladi"s repo
     # https://github.com/apalladi/covid_vaccini_monitoraggio/tree/main/dati
@@ -148,7 +192,8 @@ def get_data_from_report(auto=True, table_index=2):
 
     # Get data by age
     ages = ["12-39", "40-59", "60-79", "80+"]
-    results_ = {age: df[i::step_].stack().values for i, age in enumerate(ages)}
+    results_ = {age: df_final[i::step_].stack().values
+                for i, age in enumerate(ages)}
 
     # Load dict as df
     df_1 = pd.DataFrame(results_).T
@@ -169,5 +214,5 @@ if __name__ == "__main__":
     # Set locale to "it" to parse the month correctly
     locale.setlocale(locale.LC_ALL, "it_IT.UTF-8")
 
-    # get data
+    # Get data
     get_data_from_report()
