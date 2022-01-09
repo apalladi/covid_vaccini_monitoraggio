43acb28cce146f99013cedac618e46d51852f6a4
enricocid <enrico2588@gmail.com>
2022-01-09 21:57:18 +0100

    Corretto titolo confronto 2020-2021
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- scripts/confronto_2020_2021.py
+++ scripts/confronto_2020_2021.py
@@ -10,8 +10,8 @@ from custom.plots import apply_plot_treatment
 from custom.preprocessing_dataframe import compute_incidence, date_parser
 from custom.watermarks import add_last_updated, add_watermark
 
-x_label1 = np.arange(15, 170, 30)
-x_label2 = ["Ago", "Set", "Ott", "Nov", "Dic", "Gen"]
+x_label1 = np.arange(15, 230, 30)
+x_label2 = ["Ago", "Set", "Ott", "Nov", "Dic", "Gen", "Feb", "Mar"]
 
 
 # Importa dati
@@ -42,8 +42,7 @@ def get_epidemic_data_2020():
     # Casi e decessi 2020
     abitanti_over12 = 540*10**5
 
-    mask_2020 = (df_IT.index >= "2020-06-15") & (df_IT.index <= "2020-12-31")
-    df_2020 = df_IT[mask_2020]
+    df_2020 = df_IT.loc["2020-06-15":"2021-02-28"]
     df_2020 = df_2020[["totale_casi",
                        "deceduti"]].diff().rolling(window=30).mean()
     df_2020 = df_2020*30/(abitanti_over12/(10**5))
@@ -64,13 +63,13 @@ def get_epidemic_data_2021():
     return casi_2021_vacc, casi_2021_novacc, dec_2021_vacc, dec_2021_novacc
 
 
-def which_axe(ax):
+def which_axe(ax, title="Casi"):
     "Imposta proprietà grafici"
     ax.set_xticks(x_label1, x_label2)
-    ax.set_title("Casi mensili (media mobile 30 gg)")
+    ax.set_title(f"{title} mensili (media mobile 30 gg)")
     ax.set_ylabel("Ogni 100.000 persone per ciascun gruppo")
     ax.legend()
-    ax.set_xlim(0, 165)
+    ax.set_xlim(0, )
     ax.grid()
 
 
@@ -86,15 +85,15 @@ def plot_confronto_2020_2021(show=False):
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
     axes = ax.ravel()
 
-    axes[0].plot(xgrid_2020, casi_2020, label="2020")
-    axes[0].plot(xgrid_2021, casi_2021_vacc, label="2021 (vaccinati)")
-    axes[0].plot(xgrid_2021, casi_2021_novacc, label="2021 (non vaccinati)")
+    axes[0].plot(xgrid_2020, casi_2020, label="2020-21")
+    axes[0].plot(xgrid_2021, casi_2021_vacc, label="2021-22 (vaccinati)")
+    axes[0].plot(xgrid_2021, casi_2021_novacc, label="2021-22 (non vaccinati)")
     which_axe(axes[0])
 
-    axes[1].plot(xgrid_2020, dec_2020, label="2020")
-    axes[1].plot(xgrid_2021, dec_2021_vacc, label="2021 (vaccinati)")
-    axes[1].plot(xgrid_2021, dec_2021_novacc, label="2021 (non vaccinati)")
-    which_axe(axes[1])
+    axes[1].plot(xgrid_2020, dec_2020, label="2020-21")
+    axes[1].plot(xgrid_2021, dec_2021_vacc, label="2021-22 (vaccinati)")
+    axes[1].plot(xgrid_2021, dec_2021_novacc, label="2021-22 (non vaccinati)")
+    which_axe(axes[1], title="Decessi")
 
     # Add watermarks
     add_watermark(fig)
